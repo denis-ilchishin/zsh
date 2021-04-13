e150a29cb165f7f4f7fef24ea73d9a35db903e89
Denis Ilchishin <62346882+denis-ilchishin@users.noreply.github.com>
2021-04-13 23:31:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,4 +21,4 @@ source \$ZSH/oh-my-zsh.sh
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 " >| ~/.zshrc
 
-wget -O ~/.p10k.zsh https://github.com/genkai-online/zsh/edit/main/.p10k.zsh>
+wget -O ~/.p10k.zsh https://raw.githubusercontent.com/genkai-online/zsh/main/.p10k.zsh
