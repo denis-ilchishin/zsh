901d20ba0c95fb35eed46e8c962efd7abacc23bd
Denis Ilchishin <62346882+denis-ilchishin@users.noreply.github.com>
2021-04-13 14:43:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-apt install zsh -y
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
