db64ea8834329dc4530ba0069000c302018111eb
Denis Ilchishin <62346882+denis-ilchishin@users.noreply.github.com>
2021-04-13 15:01:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "
 if [[ -r \"\${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-\${(%):-%n}.zsh\" ]]; then
   source \"\${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-\${(%):-%n}.zsh\"
 fi
-export ZSH=\"/home/admin/.oh-my-zsh\"
+export ZSH=\"/home/$HOME/.oh-my-zsh\"
 ZSH_THEME=\"powerlevel10k/powerlevel10k\"
 plugins=(
   git
